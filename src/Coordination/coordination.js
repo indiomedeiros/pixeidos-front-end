5471903fa6b09843dea9273ac46921f4da08800f
indiomedeiros <bboyindio@gmail.com>
2021-02-26 09:54:16 -0300

    The page coordinator was implemented from the distribution of the Router paths. They are basically functions for redirecting pages from the history object

---
+++ src/Coordination/coordination.js
@@ -0,0 +1,23 @@
+export const goToHomePage = (history) => {
+  history.push("/");
+};
+
+export const goToImageDetailsPage = (history, id) => {
+  history.push(`/image/${id}`);
+};
+
+export const goToImageSearchPage = (history, dataSearch) => {
+  history.push(`/image/search/${dataSearch}`);
+};
+
+export const goToSignupPage = (history) => {
+  history.push("/signup");
+};
+
+export const goTologinPage = (history) => {
+  history.push("/login");
+};
+
+export const goToUserPage = (history) => {
+  history.push("/user");
+};
